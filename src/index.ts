dbd885e012a26b84434bb2821abf31a69dd1888d
kida-works <kida.works.1234@gmail.com>
2022-06-27 06:40:09 +0900

    Type study. number, string, boolean.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import World from './world';
+// import World from './world';
 
-const root = document.getElementById('root');
-const world = new World('Hello World Torahack!');
+// const root = document.getElementById('root');
+// const world = new World('Hello World Torahack!');
 
-world.sayHello(root);
+// world.sayHello(root);
 
+// 第3回基本の型定義
+import { primitiveSample } from './basic';
 
-
-
-
-
+primitiveSample();
