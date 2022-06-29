cc6262fc6f32d13d24b345e4cd9d5bd86808a53c
kida-works <kida.works.1234@gmail.com>
2022-06-30 00:10:56 +0900

    End of study of call signatures.

--- src/function/basic.ts
+++ src/function/basic.ts
@@ -20,3 +20,24 @@ export const logMessage4 = (message: string): void =>
 export const alwaysThrowError = (message: string): never => {
   throw new Error(message);
 };
+
+// 呼び出しシグネチャ
+// シグネチャ → 関数名と引数の数、データ型、戻り値の型を組み合わせたもの
+// ・どんな関数なのかを表現する型定義
+//　　・省略記法はアロー関数と似た形
+// ・完全な記法はオブジェクトに似た形
+
+// 呼び出しシグネチャ（省略記法）
+type LogMessage = (message: string) => void;
+export const logMessage6: LogMessage = (message) => {
+  console.log('Function basic sample 6:', message);
+};
+
+// 完全な呼び出しシグネチャ
+type FullLogMessage = {
+  (message: string): void;
+}
+export const logMessage7: LogMessage = (message) => {
+  console.log('Function basic sample 7:', message);
+};
+
