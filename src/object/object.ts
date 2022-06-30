7e23db863781f6ad01b908ddc8731fb0084d054e
kida-works <kida.works.1234@gmail.com>
2022-07-01 06:22:58 +0900

    Optional and readonly study completed.

--- src/object/object.ts
+++ src/object/object.ts
@@ -23,3 +23,25 @@ export default function objectSample() {
   };
   console.log('Object object Sample 2:', country);
 }
+
+// 特別なプロパティを扱う
+// ・オプショナル（？）のついたプロパティはあってもなくてもOK
+// ・readonlyのついたプロパティは上書きできない
+
+// オプショナルとreadonly
+const torahack: {
+  age: number;
+  lastName: string;
+  readonly firstName: string;
+  gender?: string;
+} = {
+  age: 28,
+  lastName: 'Yamada',
+  firstName: 'Tarou',
+};
+
+torahack.gender = 'male';
+torahack.lastName = 'kamado';
+// torahack.firstName = 'Tanjiro';
+
+console.log('Object object Sample 3:', torahack);
