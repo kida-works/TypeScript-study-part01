17d18f3331306f08cd75fdcb53618e771778393b
kida-works <kida.works.1234@gmail.com>
2022-07-01 06:39:12 +0900

    Study of index signatures completed.

--- src/object/object.ts
+++ src/object/object.ts
@@ -22,26 +22,45 @@ export default function objectSample() {
     name: 'United States of America',
   };
   console.log('Object object Sample 2:', country);
+
+  // 特別なプロパティを扱う
+  // ・オプショナル（？）のついたプロパティはあってもなくてもOK
+  // ・readonlyのついたプロパティは上書きできない
+
+  // オプショナルとreadonly
+  const torahack: {
+    age: number;
+    lastName: string;
+    readonly firstName: string;
+    gender?: string;
+  } = {
+    age: 28,
+    lastName: 'Yamada',
+    firstName: 'Tarou',
+  };
+
+  torahack.gender = 'male';
+  torahack.lastName = 'kamado';
+  // torahack.firstName = 'Tanjiro';
+
+  console.log('Object object Sample 3:', torahack);
+
+  // オブジェクトの柔軟な型定義
+  // インデックスシグネチャ
+  // ・オブジェクトが複数のプロパティを持つ可能性がある
+  // ・[key: T]: Uのように定義する。
+  // ・keyはstringかnumberのみ
+
+  // インデックスシグネチャ
+  const capitals: {
+    [countryName: string]: string;
+  } = {
+    Japan: 'Tokyo',
+    Korea: 'Seoul',
+  };
+
+  capitals.China = 'Beijing';
+  capitals.Canada = 'Ottawa';
+
+  console.log('Object object Sample 4:', capitals);
 }
-
-// 特別なプロパティを扱う
-// ・オプショナル（？）のついたプロパティはあってもなくてもOK
-// ・readonlyのついたプロパティは上書きできない
-
-// オプショナルとreadonly
-const torahack: {
-  age: number;
-  lastName: string;
-  readonly firstName: string;
-  gender?: string;
-} = {
-  age: 28,
-  lastName: 'Yamada',
-  firstName: 'Tarou',
-};
-
-torahack.gender = 'male';
-torahack.lastName = 'kamado';
-// torahack.firstName = 'Tanjiro';
-
-console.log('Object object Sample 3:', torahack);
