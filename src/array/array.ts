9db85bd30ef41a049b22d31945f31eb82d49372f
kida-works <kida.works.1234@gmail.com>
2022-07-04 06:37:11 +0900

    Study of type inference for arrays is completed.

--- src/array/array.ts
+++ src/array/array.ts
@@ -15,5 +15,19 @@ export default function arraySample() {
   ids.push('DEF');
   ids.push(456);
   // ids.push(true);
-  console.log('Array array sample 3:', ids)
+  console.log('Array array sample 3:', ids);
+
+  // 配列の型推論
+  const generateSameArray = () => {
+    const _sameArray = []; //any[]
+    _sameArray.push(123); //number[]
+    _sameArray.push('ABC'); //(string | number)[]
+    return _sameArray;
+  };
+
+  const sameArray = generateSameArray()
+  sameArray.push(456)
+  // sameArray.push(true)
+  console.log('Array array sample 4:', sameArray);
+
 }
