9db4de7dccf6aaa86969ba484c11a4570c626da0
kida-works <kida.works.1234@gmail.com>
2022-07-10 23:25:05 +0900

    Fixed a study error in asynchronous processing by TypeScript.

--- src/asynchronous/asyncAwait.ts
+++ src/asynchronous/asyncAwait.ts
@@ -1,4 +1,4 @@
-export default function asyncAwaitSample() {
+export default async function asyncAwaitSample() {
   const url = 'https://api.github.com/users/kida-works';
 
   type Profile = {
@@ -8,17 +8,19 @@ export default function asyncAwaitSample() {
 
   type FetchProfile = () => Promise<Profile | null>;
 
-  const fetchProfile: FetchProfile = () => {
+  // async/awaitでコールバック関数を同期的な処理に置き換える
+  const fetchProfile: FetchProfile = async () => {
     const response = await fetch(url)
       .then((res) => res)
       .catch((error) => {
         console.log(error);
         return null;
       });
-
+    // responseがnullならfetchに失敗している
     if (!response) {
       return null;
     }
+
     const json = await response
       .json()
       .then((json: Profile) => {
@@ -36,7 +38,7 @@ export default function asyncAwaitSample() {
     return json;
   };
   const profile = await fetchProfile();
-  if(profile){
+  if (profile) {
     console.log('Asynchronous async/await Sample 2:', profile);
   }
 }
